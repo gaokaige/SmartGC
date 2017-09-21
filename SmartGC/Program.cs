50769c669bc8729be05bd42daeffc7945bcae81e
gaokaige <gaokaige@aero-edu.com>
2017-09-21 17:28:02 +0800

    create

---
+++ SmartGC/Program.cs
@@ -0,0 +1,35 @@
+﻿using SmartGC.Ui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SmartGC
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            bool noInstance;
+
+            System.Threading.Mutex mutex = new System.Threading.Mutex(true, "Global\\" + System.Reflection.Assembly.GetExecutingAssembly().FullName, out noInstance);
+
+            if (noInstance)
+            {
+                Application.Run(new FormLogin());
+            }
+            else
+            {
+                MessageBox.Show("程序已经启动，请不要同时运行多次。");
+                Application.Exit();
+            }
+        }
+    }
+}
