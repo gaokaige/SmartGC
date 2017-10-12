f05d305283c54f876c19c91d9aad2f742dd69281
gaokaige <gaokaige@aero-edu.com>
2017-10-12 10:59:14 +0800

    1

---
+++ SmartGC/Ui/FormMerchant.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace SmartGC.Ui
+{
+    partial class FormMerchant
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.tbxCardNo = new System.Windows.Forms.TextBox();
+            this.tbxCustomer = new System.Windows.Forms.TextBox();
+            this.tbxAddress = new System.Windows.Forms.TextBox();
+            this.tbxPersonInCharge = new System.Windows.Forms.TextBox();
+            this.tbxPhoneNo = new System.Windows.Forms.TextBox();
+            this.tbxCreateDate = new System.Windows.Forms.TextBox();
+            this.tbxRemark = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnUnBinding = new System.Windows.Forms.Button();
+            this.btnBinding = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "备注";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "开卡时间";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 154);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "手机号";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 126);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 12);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "负责人";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 98);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(29, 12);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "地址";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(16, 70);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(53, 12);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "商户名称";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(16, 42);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 12);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "IC卡编号";
+            // 
+            // tbxCardNo
+            // 
+            this.tbxCardNo.Location = new System.Drawing.Point(79, 38);
+            this.tbxCardNo.Name = "tbxCardNo";
+            this.tbxCardNo.ReadOnly = true;
+            this.tbxCardNo.Size = new System.Drawing.Size(300, 21);
+            this.tbxCardNo.TabIndex = 11;
+            // 
+            // tbxCustomer
+            // 
+            this.tbxCustomer.Location = new System.Drawing.Point(79, 66);
+            this.tbxCustomer.Name = "tbxCustomer";
+            this.tbxCustomer.Size = new System.Drawing.Size(300, 21);
+            this.tbxCustomer.TabIndex = 12;
+            // 
+            // tbxAddress
+            // 
+            this.tbxAddress.Location = new System.Drawing.Point(79, 94);
+            this.tbxAddress.Name = "tbxAddress";
+            this.tbxAddress.Size = new System.Drawing.Size(300, 21);
+            this.tbxAddress.TabIndex = 13;
+            // 
+            // tbxPersonInCharge
+            // 
+            this.tbxPersonInCharge.Location = new System.Drawing.Point(79, 122);
+            this.tbxPersonInCharge.Name = "tbxPersonInCharge";
+            this.tbxPersonInCharge.Size = new System.Drawing.Size(300, 21);
+            this.tbxPersonInCharge.TabIndex = 14;
+            // 
+            // tbxPhoneNo
+            // 
+            this.tbxPhoneNo.Location = new System.Drawing.Point(79, 150);
+            this.tbxPhoneNo.Name = "tbxPhoneNo";
+            this.tbxPhoneNo.Size = new System.Drawing.Size(300, 21);
+            this.tbxPhoneNo.TabIndex = 15;
+            // 
+            // tbxCreateDate
+            // 
+            this.tbxCreateDate.Location = new System.Drawing.Point(79, 178);
+            this.tbxCreateDate.Name = "tbxCreateDate";
+            this.tbxCreateDate.ReadOnly = true;
+            this.tbxCreateDate.Size = new System.Drawing.Size(300, 21);
+            this.tbxCreateDate.TabIndex = 16;
+            // 
+            // tbxRemark
+            // 
+            this.tbxRemark.Location = new System.Drawing.Point(79, 206);
+            this.tbxRemark.Name = "tbxRemark";
+            this.tbxRemark.Size = new System.Drawing.Size(300, 21);
+            this.tbxRemark.TabIndex = 17;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(294, 242);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(85, 23);
+            this.btnSave.TabIndex = 18;
+            this.btnSave.Text = "保存商户信息";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(16, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "这是一张新卡";
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(18, 242);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(85, 23);
+            this.btnRead.TabIndex = 20;
+            this.btnRead.Text = "读卡";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnUnBinding
+            // 
+            this.btnUnBinding.Location = new System.Drawing.Point(110, 242);
+            this.btnUnBinding.Name = "btnUnBinding";
+            this.btnUnBinding.Size = new System.Drawing.Size(85, 23);
+            this.btnUnBinding.TabIndex = 21;
+            this.btnUnBinding.Text = "解绑IC卡";
+            this.btnUnBinding.UseVisualStyleBackColor = true;
+            this.btnUnBinding.Click += new System.EventHandler(this.btnUnBinding_Click);
+            // 
+            // btnBinding
+            // 
+            this.btnBinding.Location = new System.Drawing.Point(202, 242);
+            this.btnBinding.Name = "btnBinding";
+            this.btnBinding.Size = new System.Drawing.Size(85, 23);
+            this.btnBinding.TabIndex = 22;
+            this.btnBinding.Text = "绑定IC卡";
+            this.btnBinding.UseVisualStyleBackColor = true;
+            this.btnBinding.Click += new System.EventHandler(this.btnBinding_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(16, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(389, 12);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "绑定/解绑IC卡不会修改商户信息，只会绑定/解绑商户与原IC卡的关系。";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(16, 326);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(329, 12);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "保存商户信息不会改变商户绑定的IC卡，只能保存其他信息。";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(16, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "请注意：";
+            // 
+            // FormMerchant
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 354);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBinding);
+            this.Controls.Add(this.btnUnBinding);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbxRemark);
+            this.Controls.Add(this.tbxCreateDate);
+            this.Controls.Add(this.tbxPhoneNo);
+            this.Controls.Add(this.tbxPersonInCharge);
+            this.Controls.Add(this.tbxAddress);
+            this.Controls.Add(this.tbxCustomer);
+            this.Controls.Add(this.tbxCardNo);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMerchant";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "卡信息";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMerchant_FormClosing);
+            this.Load += new System.EventHandler(this.FormCardBinding_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox tbxCardNo;
+        private System.Windows.Forms.TextBox tbxCustomer;
+        private System.Windows.Forms.TextBox tbxAddress;
+        private System.Windows.Forms.TextBox tbxPersonInCharge;
+        private System.Windows.Forms.TextBox tbxPhoneNo;
+        private System.Windows.Forms.TextBox tbxCreateDate;
+        private System.Windows.Forms.TextBox tbxRemark;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnUnBinding;
+        private System.Windows.Forms.Button btnBinding;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
